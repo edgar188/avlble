eebe3aac60eba8aa837983644a8bbe10b24801e5
edgar <edgar.hovhannisyan08@gmail.com>
2019-11-14 12:37:41 +0400

    Created slider and some global classes

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $("#owl-demo").owlCarousel({
+        navigation: true,
+        slideSpeed: 100,
+        paginationSpeed: 200,
+        singleItem: true,
+        autoplay: true,
+        autoPlaySpeed: 1000,
+        autoPlayTimeout: 1000,
+        items : 1, 
+    });
+});
